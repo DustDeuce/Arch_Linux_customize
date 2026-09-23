ce7a9da2eb1b83fd4dd39d101059c418446aa551
AshenWolf_v2.5.7 <146616796+DustDeuce@users.noreply.github.com>
2026-09-23 21:21:13 +0400

    Create hyprland.lua
    Основной файл конфига, к которому подключены отдельные части с файлов расположенных в папке /conf

---
+++ .config/hypr/hyprland.lua
@@ -0,0 +1,11 @@
+-- // === ПОДКЛЮЧЕНИЕ МОДУЛЕЙ === //
+
+require("conf.monitors")
+require("conf.env")
+require("conf.auto_start")
+require("conf.general")
+require("conf.decoration")
+require("conf.animations")
+require("conf.input")
+require("conf.key_binds")
+require("conf.rules")
